58d90746d6a76b7557d29e3f8196b5776cab3190
Aurelie Menin <atypik77@hotmail.fr>
2022-08-24 19:17:46 +0200

    work to gameOver function

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,21 +3,26 @@ import Proposal from "./Proposal";
 
 /****** COMPONENT GAME ********/
 const Game = (props) => {
-
   const [timer, setTimer] = useState(60);
   const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false)
+
 
   useEffect(() => {
     let interval = null;
 
     interval = setInterval(() => {
-        if (timer > 0) {
-          setTimer(timer => timer - 1);
-        } 
-      }, 1000);
-      return 
-
+      if (timer > 0) {
+        setTimer((timer) => timer - 1);
+      }
+    }, 1000);
+    return;
   }, [timer]);
+
+  /* Refresh la page pour rejouer */
+   function handleClick() {
+    window.location.reload(true)
+  }
 
   /****** RENDU VISUEL ********/
   return (
@@ -28,8 +33,12 @@ const Game = (props) => {
         </div>
         <div className="main-card-game">
           <div className="timerscore">
-            <div className="timer">Temps: <b>{timer}</b></div>
-            <div className="score">Score: <b>{score}</b></div>
+            <div className="timer">
+              Temps: <b>{timer}</b>
+            </div>
+            <div className="score">
+              Score: <b>{score}</b>
+            </div>
           </div>
           {/* <h4>Cet acteur a t'il joué dans ce film ? 🍿</h4>
           <div className="section-card">
@@ -45,7 +54,14 @@ const Game = (props) => {
             <div className="button-no">✖️ NON</div>
           </div> */}
         </div>
-        <Proposal /> 
+        <div className="game-over-container">
+          <div className="game-over">
+            <h1>GAME OVER !</h1>
+            <div className="game-over-score">Score: <b>{score}</b></div>
+            <button className="button-play-again" onClick={handleClick}>REJOUER</button>
+          </div>
+        </div>
+        <Proposal />
       </div>
     </>
   );
