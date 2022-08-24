426e0fdc0c25264860ffe822f361a75745ad024f
Aurelie Menin <atypik77@hotmail.fr>
2022-08-24 12:58:57 +0200

    prepared call API

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
 import Home from "./components/Home";
+//import useSWR from "swr"; /*HOOK pr récupérer de la data URL & API */
 
+
+/****** FONCTION APP ********/
 const App = () => {
 
+  /****** RENDU VISUEL ********/
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/game" exact element={<Game />} />
+        <Route
+          path="/game"
+          exact
+          element={
+            <Game
+            />
+          }
+        />
         <Route path="*" exact element={<Home />} /> {/* Redirection Home */}
       </Routes>
     </BrowserRouter>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Game = () => {
-    return (
-        <div>
-            hello depuis game
+    
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-header">
+          <h1>MOVIE QUIZZ</h1>
         </div>
-    );
+        <div className="main-card">
+          <div className="timerscore">
+            <div className="timer">TIMER</div>
+            <div className="score">SCORE</div>
+          </div>
+          <h4>Cet acteur a t'il joué dans ce film ? 🍿</h4>
+          <div className="section-card">
+            <div className="img-container">
+              <div className="image">IMAGE ICI</div>
+            </div>
+            <div className="img-container">
+              <div className="image">IMAGE ICI</div>
+            </div>
+          </div>
+          <div className="section-card">
+            <div className="button-yes">✔️ OUI</div>
+            <div className="button-no">✖️ NON</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Game;
