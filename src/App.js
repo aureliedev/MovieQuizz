a84cb5fedaf84a4aeadae8ebb7a38083a8114d5b
Aurelie Menin <atypik77@hotmail.fr>
2022-08-24 13:09:29 +0200

    add call API & array

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
 import Home from "./components/Home";
-//import useSWR from "swr"; /*HOOK pr récupérer de la data URL & API */
+import useSWR from "swr"; /*HOOK pr récupérer de la data URL & API */
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 /****** FONCTION APP ********/
 const App = () => {
+
+  /*Récup les 20 acteurs les + populaires */
+  const { data, error } = useSWR(
+    "https://api.themoviedb.org/3/person/popular?api_key=eb553d0ae0d677efb0e511568c1b789a&language=en-US&page=1",
+    fetcher
+  );
+
+  const actors = [];
+  const movies = [];
+
+  /* Si erreur on STOP */
+  if (error) return <div>Erreur de chargement</div>;
+  if (!data) return <div>Chargement...</div>;
+  /* Si OK on récupère la data */
+  if (data) {
+    const actorsResults = [...data.results];
+
+   
+  };
 
   /****** RENDU VISUEL ********/
   return (
@@ -17,8 +37,7 @@ const App = () => {
           path="/game"
           exact
           element={
-            <Game
-            />
+            <Game/>
           }
         />
         <Route path="*" exact element={<Home />} /> {/* Redirection Home */}
